cf714756e38359e2fbbc027bfea36a19a0fe8600
Sayed Kaif <142001584+sayedakhib@users.noreply.github.com>
2024-06-05 06:57:45 +0530

    Add files via upload

---
+++ food_order/ofos/partials-front/footer.php
@@ -0,0 +1,34 @@
+ <!-- social Section Starts Here -->
+ <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png" /></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png" /></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>&copy;All rights reserved.</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+    <!-- JavaScript for toggling menu -->
+    <script>
+        document.querySelector('.hamburger').addEventListener('click', function () {
+            document.querySelector('.menu').classList.toggle('active');
+        });
+    </script>
+</body>
+
+</html>
---
+++ food_order/ofos/partials-front/menu.php
@@ -0,0 +1,49 @@
+<?php include('config/constants.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Logo">
+                    <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+            <div class="hamburger">
+                <img src="https://img.icons8.com/ios/30/000000/menu--v1.png" alt="Menu">
+            </div>
+            <div class="menu text-right" id="menu">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="categories.php">Categories</a>
+                    </li>
+                    <li>
+                        <a href="foods.php">Foods</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+
+    <!-- Navbar Section Ends Here -->
